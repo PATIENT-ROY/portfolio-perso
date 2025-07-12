eeb7c91e087681d5dc6dcc27a55d4b8224289f04
Patient-Roy <roy-mbaya@mail.ru>
2025-07-13 01:19:59 +0500

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experiences from "./components/Experiences";
@@ -8,29 +9,42 @@ import Projects from "./components/Projects";
 import ScrollProgress from "./components/ScrollProgress";
 import ScrollToTop from "./components/ScrollToTop";
 import SEOHead from "./components/SEOHead";
+import WelcomeScreen from "./components/WelcomeScreen";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 export default function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const handleWelcomeComplete = () => {
+    setShowWelcome(false);
+  };
+
   return (
     <ThemeProvider>
       <SEOHead />
-      <ScrollProgress />
-      <ScrollToTop />
-      <div>
-        <div className="p-5 md:px-[15%]">
-          <Navbar />
-          <Home />
-        </div>
-        <About />
-        <div className="p-5 md:px-[15%]">
-          <Experiences />
-          <div className="mt-16">
-            <Projects />
+      {showWelcome ? (
+        <WelcomeScreen onComplete={handleWelcomeComplete} />
+      ) : (
+        <>
+          <ScrollProgress />
+          <ScrollToTop />
+          <div>
+            <div className="p-5 md:px-[15%]">
+              <Navbar />
+              <Home />
+            </div>
+            <About />
+            <div className="p-5 md:px-[15%]">
+              <Experiences />
+              <div className="mt-16">
+                <Projects />
+              </div>
+            </div>
+            <Contact />
+            <Footer />
           </div>
-        </div>
-        <Contact />
-        <Footer />
-      </div>
+        </>
+      )}
     </ThemeProvider>
   );
 }
